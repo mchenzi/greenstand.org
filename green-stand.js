c3d90fc13f95228cf30f9698967f6f1943f7e695
Alan Cole <alantuneit@gmail.com>
2017-05-31 13:59:47 -0600

    getting set up

---
+++ green-stand.js
@@ -0,0 +1,29 @@
+require('colors');
+var express         = require('express'),
+    bodyParser      = require('body-parser'),
+    mongoose        = require('mongoose'),
+    morgan          = require('morgan')('dev'),
+    Routes          = require('./routes'),
+    fs              = require('fs');
+    // clientSessions  = require('client-sessions');
+
+// connect to DB
+mongoose.connect('mongodb://localhost/green-stand');
+
+var app = express();
+
+
+// Middleware
+app.use(
+  express.static('public'),
+  bodyParser.urlencoded({extended: true}),
+  bodyParser.json()
+);
+
+// give routes.js express app object
+Routes(app);
+
+app.server - app.listen(3000, ()=> {
+  console.log("Server running on port 3000".america);
+  console.log("WOOF".zebra);
+})
---
+++ routes.js
@@ -0,0 +1,18 @@
+var GreenCtrl = require('./controllers/green-control');
+
+
+module.exports = (app) => {
+
+  app.get('/', (req, res)=>{
+    res.sendFile('index.html', {root : './public'});
+  });
+
+  app.get('/api/musician', GreenCtrl.get);
+  app.get('/api/musician/:id', GreenCtrl.get);
+  app.get('/api/me', GreenCtrl.loggedIn);
+  app.post('/api/musician', GreenCtrl.create);
+  app.post('/api/login', GreenCtrl.login);
+  app.put('/api/musician/:id', GreenoCtrl.update);
+  // app.put('/api/remove/:id', )
+
+}
