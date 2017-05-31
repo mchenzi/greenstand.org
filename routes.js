c923e770147c5ef76f3378c12ab60fa916c46d3e
Alan Cole <alantuneit@gmail.com>
2017-05-31 14:28:34 -0600

    setting up

--- routes.js
+++ routes.js
@@ -7,12 +7,12 @@ module.exports = (app) => {
     res.sendFile('index.html', {root : './public'});
   });
 
-  app.get('/api/musician', GreenCtrl.get);
-  app.get('/api/musician/:id', GreenCtrl.get);
-  app.get('/api/me', GreenCtrl.loggedIn);
-  app.post('/api/musician', GreenCtrl.create);
-  app.post('/api/login', GreenCtrl.login);
-  app.put('/api/musician/:id', GreenoCtrl.update);
+  // app.get('/api/musician', GreenCtrl.get);
+  // app.get('/api/musician/:id', GreenCtrl.get);
+  // app.get('/api/me', GreenCtrl.loggedIn);
+  // app.post('/api/musician', GreenCtrl.create);
+  // app.post('/api/login', GreenCtrl.login);
+  // app.put('/api/musician/:id', GreenoCtrl.update);
   // app.put('/api/remove/:id', )
 
 }
