374c1ce73ccd27ca38255a0f57a0f8f7d78eb535
Alan Cole <alantuneit@gmail.com>
2017-08-21 00:48:15 -0600

    basic home - about pages mock-up text and most styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,11 +8,30 @@ declare var $: any;
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  mobileMenu: Boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    $(document).foundation();
+    // $(document).foundation();
   }
 
+  // tried the method below for responsive screen sizing but doesn't work :(
+  // checkWindowWidth(window): Boolean {
+  //   const medScreen: Number = 640; // taken from Zurb-Foundation css breakpoints
+  //   if (window.innerWidth < medScreen) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  navBtnClick() {
+    this.mobileMenu = !this.mobileMenu;
+    console.log('Click for mobileMenu show up!', this.mobileMenu);
+  }
+  menuBtnClick() {
+    setTimeout(()=> this.mobileMenu = !this.mobileMenu, 750);
+    console.log('Click for mobileMenu to go away', this.mobileMenu);
+  }
 }
